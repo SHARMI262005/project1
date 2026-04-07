8ce1c7188d07cbe494ddc54cff4a832338d8ac72
logan <loganathanvms@gmail.com>
2026-04-07 11:45:56 +0530

    main . java

---
+++ main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to My Java Application 🚀");
+
+        // Simple variables
+        String name = "User";
+        int age = 21;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        // Simple condition
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are not an adult");
+        }
+    }
+}
